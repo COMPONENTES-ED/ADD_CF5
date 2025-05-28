3e3b0eeab8530fd6b78fef14b28721dfdc1545af
yesnapoleondynamite <yesnapoleondynamite@gmail.com>
2025-05-28 08:19:26 -0500

    Ajuste_actividad

--- src/pages/Actividad.vue
+++ src/pages/Actividad.vue
@@ -10,7 +10,7 @@
               img(src="@/assets/bullets/Seleccion_Multiple.svg", alt="Texto que describa la imagen")
         .col-12.col-md-8.col-lg-9
           .titulo-segundo
-            h2 Prueba de conocimientos sobre Limpieza y transformación de datos: técnicas y herramientas informáticas.
+            h2 Prueba de conocimientos sobre Limpieza y transformación de datos: técnicas y herramientas informáticas
           p.mb-4 Validar el conocimiento adquirido sobre Limpieza y transformación de datos. Esto se trabaja a partir de un conjunto de preguntas con opción de respuesta falso o verdadero, con el propósito de buscar una dinámica de razonamiento ágil sobre opciones cerradas y reafirmar un conocimiento declarado dentro del componente.
           .tarjeta.actividad.p-3
             .row.justify-content-around.align-items-center            
